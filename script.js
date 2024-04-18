e7afc7dd74311af02f8db3794ca9a0b1e369ceec
Nicolas Llancaqueo <n.llancaqueo01@ufromail.cl>
2024-04-18 13:22:35 -0400

    actualizacion

--- script.js
+++ script.js
@@ -11,3 +11,33 @@ document.getElementById("login-form").addEventListener("submit", function(event)
       document.getElementById("error-message").innerText = "error, usuario no encontrado";
     }
   });
+
+  /*document.getElementById("login-form").addEventListener("submit", async function(event) {
+  event.preventDefault(); 
+
+  var nombre = document.getElementById("name").value;
+  var correo = document.getElementById("email").value;
+  var numero = document.getElementById("number").value;
+
+  try {
+      // Realiza una solicitud POST a la API REST
+      const response = await axios.post('https://jsonplaceholder.typicode.com/posts', {
+          nombre: nombre,
+          correo: correo,
+          numero: numero
+      });
+
+      // Verifica la respuesta de la API
+      if (response.status === 200) {
+          // Si la solicitud fue exitosa, redirige a otra página
+          window.location.href = "login.html";
+      } else {
+          // Si la solicitud no fue exitosa, muestra un mensaje de error
+          document.getElementById("error-message").innerText = "Error, usuario no encontrado";
+      }
+  } catch (error) {
+      // Si ocurre un error durante la solicitud, muestra un mensaje de error
+      console.error('Error al enviar el formulario:', error);
+      document.getElementById("error-message").innerText = "Error al enviar el formulario";
+  }
+}); */
